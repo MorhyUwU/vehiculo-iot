58cf311b1615370c58826517a817dba937469e87
Hector efrain De la rosa Enciso <144378700+MorhyUwU@users.noreply.github.com>
2025-06-01 20:33:56 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -40,7 +40,7 @@ document.addEventListener('DOMContentLoaded', async () => {
             };
 
             try {
-                const response = await fetch('http://44.202.189.236:5000/api/devices', {
+                const response = await fetch('http://44.193.226.55:5000/api/devices', {
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json'
